f8b3460de3aa39d6f17d632904e17e178a8046e5
ramiel.yang <ramiel.yang@tinno.com>
2016-01-22 14:12:05 +0800

    <BUG><FCCBM-486><L5221_6.0_V1.1:3901 camera驱动提交>
    Change-Id: I03a7cef1dd11fabdf1c67cdd90ad0d3f70302191

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -8,7 +8,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- */
+ */ 
 #include "msm_sensor.h"
 #include "msm_sd.h"
 #include "camera.h"
