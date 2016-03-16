fea9f26cddc41b634288384f8ee0de941e9852e8
lion.li <lion.li@tinno.com>
2016-03-16 17:41:19 +0800

    <REQ><FCCBM-768><L5221_6.0_V1.1:wiko统一需求版本>
    Change-Id: I53006eb389389749409ea9e5d888818c69ad0f4c

--- init/main.c
+++ init/main.c
@@ -86,7 +86,13 @@
 #ifdef CONFIG_X86_LOCAL_APIC
 #include <asm/smp.h>
 #endif
-
+//JSTINNO_SRC lion.li, DATE20151226, NOTE, WIKO_UNIFY START
+#define WIKO_UNIFY_TYPE
+#ifdef WIKO_UNIFY_TYPE
+#include <linux/of_device.h>
+#include <asm/uaccess.h>
+#endif
+//JSTINNO_SRC lion.li, WIKO_UNIFY END
 static int kernel_init(void *);
 
 extern void init_IRQ(void);
@@ -763,6 +769,88 @@ static void __init do_initcalls(void)
 	for (level = 0; level < ARRAY_SIZE(initcall_levels) - 1; level++)
 		do_initcall_level(level);
 }
+//JSTINNO_SRC lion.li, DATE20151226, NOTE, WIKO_UNIFY START
+#ifdef WIKO_UNIFY_TYPE
+//static struct proc_dir_entry *device_info_entry;
+//JSTINNO_SRC lion.li, WIKO_UNIFY END
+
+DEF_TINNO_DEV_INFO(Market_Area);
+DEF_TINNO_DEV_INFO(Proximity_sensor);
+DEF_TINNO_DEV_INFO(Light_sensor);
+DEF_TINNO_DEV_INFO(Gyroscope_sensor);
+DEF_TINNO_DEV_INFO(Acceleration_sensor);
+DEF_TINNO_DEV_INFO(Magnetic_sensor);
+DEF_TINNO_DEV_INFO(OTG_open);
+DEF_TINNO_DEV_INFO(Hall);
+
+
+extern char* saved_command_line;
+
+#define TINNO_CMD_CONFIG_SIZE 8        //if need extend, please modify CMD_BUF&TINNO_CMD_CONFIG_SIZE only
+char * CMD_BUF[]={
+"Market_Area=",
+"Proximity_sensor=",
+"Light_sensor=",
+"Gyroscope_sensor=",
+"Acceleration_sensor=",
+"Magnetic_sensor=",
+"OTG_open=",
+"Hall=",
+};
+
+int tinno_platform_adapter(void)
+{
+	char temp_buf[TINNO_CMD_CONFIG_SIZE][32] = {{0},{0},{0},{0},{0},{0},{0},{0}};
+	char *p, *q;
+	int i;
+	
+	//device_info_entry=proc_mkdir("Tinno_devinfo",NULL);
+	CAREAT_TINNO_DEV_INFO(Market_Area);
+	CAREAT_TINNO_DEV_INFO(Proximity_sensor);
+	CAREAT_TINNO_DEV_INFO(Light_sensor);
+	CAREAT_TINNO_DEV_INFO(Gyroscope_sensor);
+	CAREAT_TINNO_DEV_INFO(Acceleration_sensor);
+	CAREAT_TINNO_DEV_INFO(Magnetic_sensor);
+	CAREAT_TINNO_DEV_INFO(OTG_open);
+	CAREAT_TINNO_DEV_INFO(Hall);		//if need extend, please add behind
+	
+//	SET_DEVINFO_STR(Market_Area,"101");      //test
+
+	for(i=0; i < TINNO_CMD_CONFIG_SIZE; i ++ ){
+		p = strstr(saved_command_line, CMD_BUF[i]);
+		if(p == NULL){
+			printk("cmdline do not find tinno dev config %d \n", i);
+			break;
+		}
+		p += strlen(CMD_BUF[i]);
+		if((p - saved_command_line) > strlen(saved_command_line+1)){
+			printk("cmdline find tinno dev config error %d \n", i);
+			break;
+		}
+
+		q = p;
+		while(*q != ' ' && *q != '\0')
+			q++;
+		if((int)(q-p) > 32){
+			printk("cmdline find tinno dev config length error %d \n", i);
+			break;
+		}
+		
+		memset((void*)temp_buf[i], 0, sizeof(temp_buf[i]));
+		strncpy((char*)temp_buf[i], (const char*)p, (int)(q-p));
+	}
+	SET_DEVINFO_STR(Market_Area,temp_buf[0]);
+	SET_DEVINFO_STR(Proximity_sensor,temp_buf[1]);
+	SET_DEVINFO_STR(Light_sensor,temp_buf[2]);
+	SET_DEVINFO_STR(Gyroscope_sensor,temp_buf[3]);
+	SET_DEVINFO_STR(Acceleration_sensor,temp_buf[4]);
+	SET_DEVINFO_STR(Magnetic_sensor,temp_buf[5]);
+	SET_DEVINFO_STR(OTG_open,temp_buf[6]);
+	SET_DEVINFO_STR(Hall,temp_buf[7]);			//if need extend, please add behind
+
+	return 0;
+}
+#endif
 
 /*
  * Ok, the machine is now initialized. None of the devices
@@ -816,6 +904,13 @@ static noinline void __init kernel_init_freeable(void);
 static int __ref kernel_init(void *unused)
 {
 	kernel_init_freeable();
+    
+//JSTINNO_SRC lion.li, DATE20151226, NOTE, WIKO_UNIFY START
+#ifdef WIKO_UNIFY_TYPE
+     tinno_platform_adapter();
+#endif
+//JSTINNO_SRC lion.li, WIKO_UNIFY END
+
 	/* need to finish all async __init code before freeing the memory */
 	async_synchronize_full();
 	free_initmem();
